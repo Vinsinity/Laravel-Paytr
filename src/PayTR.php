377695fbf12aa45dce50ef75043ad10537ba9470
Erhan Hangül <vinsinity@dokun.org>
2024-05-13 12:53:39 +0300

    Construst changed

--- src/PayTR.php
+++ src/PayTR.php
@@ -18,18 +18,35 @@ class PayTR {
     private $credentials;
     private $options;
 
-    public function __construct($client, $credentials, $options = []) {
+    // public function __construct($client, $credentials, $options = []) {
+    //     $this->client = $client;
+    //     $this->credentials = $credentials;
+    //     $this->options = $options;
+    // }
+
+    public function setClient($client)
+    {
         $this->client = $client;
+    }
+
+    public function setCredentials($credentials)
+    {
         $this->credentials = $credentials;
+    }
+    
+    public function setOptions($options)
+    {
         $this->options = $options;
     }
-        public function createPayment(Payment $payment)
+
+    public function createPayment(Payment $payment)
     {
         return $payment->setClient($this->client)
             ->setCredentials($this->credentials)
             ->setOptions($this->options)
             ->create();
     }
+
 
     public function payment()
     {
--- src/PayTRServiceProvider.php
+++ src/PayTRServiceProvider.php
@@ -11,37 +11,13 @@ class PayTRServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $configPath = __DIR__ . '/../config/paytr.php';
-        $this->publishes([$configPath => config_path('paytr.php')], 'config');
+        
     }
 
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__ . '/../config/paytr.php', 'paytr');
         $this->app->bind('PayTR', function($app) {
-            $config = config('paytr');
-            if (is_null($config)) {
-                throw InvalidConfigException::configNotFound();
-            }
-
-            $client = new Client([
-                'base_uri' => $config['options']['base_uri'],
-                'timeout' => $config['options']['timeout'],
-            ]);
-            return new PayTR($client, $config['credentials'], $config['options']);
+            return new PayTR();
         });
-        /*
-        $this->app->singleton(Paytr::class, function ($app) {
-            $config = config('paytr');
-            if (is_null($config)) {
-                throw InvalidConfigException::configNotFound();
-            }
-
-            $client = new Client([
-                'base_uri' => $config['options']['base_uri'],
-                'timeout' => $config['options']['timeout'],
-            ]);
-            return new PayTR($client, $config['credentials'], $config['options']);
-        });*/
     }
 }
